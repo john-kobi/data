53a842fb027ac355c4e3ea6c32bd8b49fd052bb4
John Molesworth <jp.molesworth@gmail.com>
2023-03-23 13:30:57 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+# Create venv from command line
+# 1. mkdir 'name of new dir'
+# 2. One folder up python3.10 -m  venv nameofnewdir/venv
+# 3. cd venv/bin/source activate
+# 4. cd venv/bin/deactivate
+
